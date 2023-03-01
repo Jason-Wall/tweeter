b0b91d3445866cc57df96f192a5d58020f8fd7d3
Jason-Wall <jason.waldick@gmail.com>
2023-03-01 10:03:48 -0700

    setup ajax post for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,5 @@
+
+
 const tweetDB = [
   {
     user: {
@@ -23,50 +25,76 @@ const tweetDB = [
   }
 ];
 
-$(document).ready(() => {
-  const populateOldTweets = (tweet) => {
-    const container = $('<div>').addClass('tweet');
 
-    //header:
-    const header = $('<header>').addClass('tweet-header');
-    const headerLeftSpan = $('<span>').addClass('header-left-span');
-    const avatar = $('<img>').attr('src', tweet.user.avatars);
-    const userName = $('<span>').text(tweet.user.name);
-    const handle = $('<span>').text(tweet.user.handle).addClass('handle');
-    headerLeftSpan.append(avatar, userName);
-    header.append(headerLeftSpan, handle);
-
-    //tweet-text:
-    const tweetText = $('<article>').addClass('tweet-text').text(`${tweet.content.text}`);
-
-    //footer:
-    const footer = $('<footer>').addClass('tweet-footer');
-    const dateSpan = $('<span>').text(`${tweet.created_at}`);
-    const footerRightSpan = $('<span>').addClass('footer-right-span');
-    const iconflag = $('<i>').addClass('icon icon-color fa-solid fa-flag');
-    const iconretweet = $('<i>').addClass('icon icon-color fa-solid fa-retweet');
-    const iconheart = $('<i>').addClass('icon icon-color fa-solid fa-heart');
-
-    footerRightSpan.append(iconflag, iconheart, iconretweet);
-    footer.append(dateSpan, footerRightSpan);
-
-    //Full assembly:
-    container.append(header, tweetText, footer);
-
-    // Event listeners
-    const icons = [iconflag, iconretweet, iconheart];
-    for (let icon of icons) {
-      icon.on('mouseover mouseout', function () {
-        icon.toggleClass('hover-color icon-color');
-      });
-    };
-    return container;
-  };
+const populateTweet = (tweet) => {
+  const container = $('<article>').addClass('tweet');
   
-  for (let entry of tweetDB) {
-    console.log(entry.user.name);
-    $('.old-tweets').append(populateOldTweets(entry));
+  //header elements:
+  const header = $('<header>').addClass('tweet-header');
+  const headerLeftSpan = $('<span>').addClass('header-left-span');
+  const avatar = $('<img>').attr('src', tweet.user.avatars);
+  const userName = $('<span>').text(tweet.user.name);
+  const handle = $('<span>').text(tweet.user.handle).addClass('handle');
+
+  headerLeftSpan.append(avatar, userName);
+  header.append(headerLeftSpan, handle);
+  
+  //tweet-text elements:
+  const tweetText = $('<div>').addClass('tweet-text').text(`${tweet.content.text}`);
+  
+  //footer elements:
+  const footer = $('<footer>').addClass('tweet-footer');
+  const dateSpan = $('<span>').text(`${tweet.created_at}`);
+  const footerRightSpan = $('<span>').addClass('footer-right-span');
+  const iconflag = $('<i>').addClass('icon icon-color fa-solid fa-flag');
+  const iconretweet = $('<i>').addClass('icon icon-color fa-solid fa-retweet');
+  const iconheart = $('<i>').addClass('icon icon-color fa-solid fa-heart');
+  
+  footerRightSpan.append(iconflag, iconheart, iconretweet);
+  footer.append(dateSpan, footerRightSpan);
+  
+  //Full assembly:
+  container.append(header, tweetText, footer);
+  
+  // Event listeners
+  const icons = [iconflag, iconretweet, iconheart];
+  for (let icon of icons) {
+    icon.on('mouseover mouseout', function () {
+      icon.toggleClass('hover-color icon-color');
+    });
   };
+
+
+  return container;
+};
+
+const renderTweets = (tweetDB) => {
+  for (let tweet of tweetDB) {
+    $('.old-tweets').append(populateTweet(tweet));
+  };
+}
+
+const submitTweetAJAX = () => {
+  const tweetForm = $('#tweet-form');
+  tweetForm.on('submit', function (event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: serializedData
+    })
+    .then(console.log('successful upload'));
+  });
+};
+
+// const loadTweetsAJAX = () => {
+//   $.get('/tweets',)
+// }
+
+$(document).ready(() => {
+  renderTweets(tweetDB);
+  submitTweetAJAX();
 });
 
 
