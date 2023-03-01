fa4f75b863d6ee3eec5ddcaad2aa5c489c6cf747
Jason-Wall <jason.waldick@gmail.com>
2023-03-01 16:10:41 -0700

    Sterilize code against XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,76 +1,36 @@
-
-
-const tweetDB = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1677460437107
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1677546837107
-  }
-];
-
-
 const populateTweet = (tweet) => {
-  const container = $('<article>').addClass('tweet');
-  
-  //header elements:
-  const header = $('<header>').addClass('tweet-header');
-  const headerLeftSpan = $('<span>').addClass('header-left-span');
-  const avatar = $('<img>').attr('src', tweet.user.avatars);
-  const userName = $('<span>').text(tweet.user.name);
-  const handle = $('<span>').text(tweet.user.handle).addClass('handle');
+  const { user, content, created_at } = tweet;
+  let tweetContainer = $(`
+  <article class="tweet">
+    <header class="tweet-header">
+      <span class="header-left-span">
+        <img src="${user.avatars}">
+        <span>${user.name}</span>
+      </span>
+      <span class="handle">${user.handle}</span>
+    </header>
+    <div>${escape(content.text)}</div>
+    <footer class="tweet-footer">
+      <span>${timeago.format(created_at)}</span>
+      <span class="footer-right-span">
+        <i class="icon icon-color fa-solid fa-flag"></i>
+        <i class="icon icon-color fa-solid fa-heart"></i>
+        <i class="icon icon-color fa-solid fa-retweet"></i>
+      </span>
+    </footer>
+  </article>`);
+  return tweetContainer;
+}; 
 
-  headerLeftSpan.append(avatar, userName);
-  header.append(headerLeftSpan, handle);
-  
-  //tweet-text elements:
-  const tweetText = $('<div>').addClass('tweet-text').text(`${tweet.content.text}`);
-  
-  //footer elements:
-  const footer = $('<footer>').addClass('tweet-footer');
-  const dateSpan = $('<span>').text(`${tweet.created_at}`);
-  const footerRightSpan = $('<span>').addClass('footer-right-span');
-  const iconflag = $('<i>').addClass('icon icon-color fa-solid fa-flag');
-  const iconretweet = $('<i>').addClass('icon icon-color fa-solid fa-retweet');
-  const iconheart = $('<i>').addClass('icon icon-color fa-solid fa-heart');
-  
-  footerRightSpan.append(iconflag, iconheart, iconretweet);
-  footer.append(dateSpan, footerRightSpan);
-  
-  //Full assembly:
-  container.append(header, tweetText, footer);
-  
-  // Event listeners
-  const icons = [iconflag, iconretweet, iconheart];
-  for (let icon of icons) {
-    icon.on('mouseover mouseout', function () {
-      icon.toggleClass('hover-color icon-color');
-    });
-  };
-
-
-  return container;
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
 };
 
 const renderTweets = (tweetDB) => {
   for (let tweet of tweetDB) {
-    $('.old-tweets').append(populateTweet(tweet));
+    $('.old-tweets').prepend(populateTweet(tweet));
   };
 }
 
@@ -83,23 +43,26 @@ const submitTweetAJAX = () => {
       method: 'POST',
       url: '/tweets',
       data: serializedData
+    }).then(() => {
+      loadTweetsAJAX((results) => {
+        const newestTweet = [results.pop()];
+        $('.tweet-text').val('');
+        renderTweets(newestTweet);
+
+      })
     })
-    .then(console.log('successful upload'));
   });
 };
 
-const loadTweetsAJAX = () => {
+const loadTweetsAJAX = (callback) => {
   $.ajax({
     method: 'GET',
     url: '/tweets'
   })
-  .then((response) => {renderTweets(response)});
-}
+    .then((result) => {callback(result)})
+};
 
 $(document).ready(() => {
-  loadTweetsAJAX();
   submitTweetAJAX();
+  loadTweetsAJAX(renderTweets);
 });
-
-
-
